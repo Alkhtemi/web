1fafa9b4654f5c57952c50d5eea2215cab0cc004
Iuri97 <32301585+Iuri97@users.noreply.github.com>
2019-04-17 15:30:29 +0100

    database and register initial setup

---
+++ src/main/java/main/Runner.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import db.H2User;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import servlet.MenuServlet;
+import servlet.UserServlet;
+
+/**
+ *
+ * @author Iuri
+ */
+public class Runner {
+    
+
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+
+    private static final int PORT = 9003;
+    
+      private final H2User h2User;
+
+    
+
+  
+
+    private Runner() {
+       
+        h2User = new H2User();
+    }
+
+    private void start() throws Exception {
+        Server server = new Server(PORT);
+
+        ServletContextHandler handler = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS);
+        handler.setContextPath("/");
+        handler.setInitParameter("org.eclipse.jetty.servlet.Default." + "resourceBase", "src/main/resources/webapp");
+        
+        handler.addServlet(new ServletHolder(new UserServlet(h2User)), "/register.html");
+        handler.addServlet(new ServletHolder(new UserServlet(h2User)), "/add"); // we post to here
+
+        DefaultServlet ds = new DefaultServlet();
+        handler.addServlet(new ServletHolder(ds), "/");
+
+         MenuServlet menuServlet = new MenuServlet();
+        handler.addServlet(new ServletHolder(menuServlet), "/menu");
+
+
+        server.start();
+        LOG.info("Server started, will run until terminated");
+        server.join();
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            LOG.info("server starting...");
+            new Runner().start();
+        } catch (Exception e) {
+            LOG.error("Unexpected error running: " + e.getMessage());
+        }
+    }
+}
